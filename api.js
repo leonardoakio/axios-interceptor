93f544418657a8269e14a81d018016bb19e8eeb1
leonardo.hamada <akioleonardo@gmail.com>
2024-04-08 10:44:06 -0300

    interceptor update

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
-import dayjs from 'dayjs';
 
-const baseURL = import.meta.env.VITE_API_URL
+const frontURL = "http://localhost:8081/";
+const baseURL = "http://localhost:8080/";
 
 const api = axios.create({
    baseURL: baseURL,
@@ -12,52 +12,40 @@ api.interceptors.request.use(async req => {
    let accessToken = Cookies.get('access_token') || null;
    let refreshToken = localStorage.getItem('refresh_token') || null;
 
-   if (req.url === 'api/v1/refresh-token') {
+   if (req.url === 'api/v1/credentials/refresh-token') {
       return req; // Deixa seguir o fluxo de renovação de token
    }
 
-   if (req.url === 'api/v1/loading') {
-      return req; // Deixa seguir o fluxo recebimento de authorization code
+   if (req.url === 'api/v1/credentials/login') {
+      return req; // Deixa seguir o fluxo de renovação de token
    }
 
-   if (req.url === 'api/v1/access-token') {
-      return req; // Deixa seguir fluxo de troca de tokens
-   }
-
-   if (!accessToken) {
-      const refreshToken = localStorage.getItem('refresh_token') || null;
-
-      if (!refreshToken) {
-         window.location.href = `${baseURL}api/v1/auth-login`;
+   if (!accessToken || accessToken === 'undefined') {
+      if (!refreshToken || refreshToken === 'undefined') {
+         window.location.href = `${frontURL}invoice`;
          return req;
       }
 
-      //TODO Testar
-      const isExpired = dayjs.unix(refreshToken.exp).isBefore(dayjs());
-      if (isExpired) {
-         window.location.href = `${baseURL}api/v1/auth-login`;
+      const isRefreshTokenExpired = (refreshToken) => {
+         const decodedToken = JSON.parse(Buffer.from(refreshToken.split('.')[1], 'base64').toString());
+         return Date.now() >= decodedToken.exp * 1000;
+      };
+
+      if (isRefreshTokenExpired) {
+         window.location.href = `${frontURL}invoice`;
          return req;
       }
 
-      const response = await api.post(`api/v1/refresh-token`, {
+      const response = await api.post(`api/v1/credentials/refresh-token`, {
          refresh_token: refreshToken
       });
 
-      console.log("newAccessToken: ", response.data.access_token)
-
       localStorage.setItem('refresh_token', response.data.refresh_token)
       Cookies.set('access_token', response.data.access_token, { expires: new Date(Date.now() + 5 * 60 * 1000) });
-
-      accessToken = Cookies.get('access_token') || null; // Atualiza o accessToken
    }
 
    if (!refreshToken) {
-      window.location.href = `${baseURL}api/v1/auth-login`;
-      return req;
-   }
-
-   if (accessToken === 'undefined' || refreshToken === 'undefined') {
-      window.location.href = `${baseURL}api/v1/auth-login`;
+      window.location.href = `${frontURL}invoice`;
       return req;
    }
 
